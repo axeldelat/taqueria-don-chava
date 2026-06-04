8098c39679a270b0b3d4cd866b4ca59200f037d2
v0agent <it+v0agent@vercel.com>
2026-06-04 17:59:05 +0000

    feat: replace gtag.js with Google Tag Manager and add noscript tag
    Co-authored-by: Axel de la Torre <19143413+axeldelat@users.noreply.github.com>

--- pages/_document.js
+++ pages/_document.js
@@ -20,23 +20,29 @@ export default function Document() {
         <meta name="twitter:description" content="Una vida taqueando." />
         <meta name="twitter:image" content="https://taqueria-don-chava.vercel.app/og-image.jpg" />
 
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-G9YX8E559E"
-        />
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-G9YX8E559E');
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TQGT459M');`,
           }}
         />
+        {/* End Google Tag Manager */}
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TQGT459M"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
